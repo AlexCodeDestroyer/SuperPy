b6aa23a31d4b8e19eb7ed35a5e8206ac83527d5d
AlexCodeDestroyer <45488563+AlexCodeDestroyer@users.noreply.github.com>
2018-11-30 07:12:52 -0600

    Add files via upload

---
+++ superpyinastaller.py
@@ -0,0 +1,12 @@
+def superPyInstaller():
+	import urllib.request
+	import sys
+	import os
+	selfPath = os.getcwd()
+	os.mkdir("/superpy/")
+	url = "https://github.com/AlexCodeDestroyer/SuperPy/blob/master/superpy1.py"
+	urllib.request.urlretrieve(url, "/superpy/superpy1.py")
+	url = "https://github.com/AlexCodeDestroyer/SuperPy/blob/master/superpy2.py"
+	urllib.request.urlretrieve(url, "/superpy/superpy2.py")
+	url = "https://github.com/AlexCodeDestroyer/SuperPy/blob/master/superpy3.py"
+	urllib.request.urlretrieve(url, "/superpy/superpy3.py")
